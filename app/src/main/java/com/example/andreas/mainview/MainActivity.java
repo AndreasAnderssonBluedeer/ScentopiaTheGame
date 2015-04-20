f800a3cb4cf51b7b7e15f6ff828e1361bec8bc7b
Andreas Andersson <bambismurfen@gmail.com>
2015-04-20 20:35:32 +0200

    Fixat Kartscroll

--- app/src/main/java/com/example/andreas/mainview/MainActivity.java
+++ app/src/main/java/com/example/andreas/mainview/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.andreas.mainview;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Color;
+import android.media.Image;
 import android.os.CountDownTimer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 
@@ -15,7 +20,9 @@ public class MainActivity extends Activity {
 
     private RelativeLayout layout;
     private ProgressBar loadProg;
-
+    private HorizontalScrollView map;
+    private ImageView imgMap;
+    private Context context =this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +33,10 @@ public class MainActivity extends Activity {
 
         loadProg=(ProgressBar)findViewById(R.id.loadProg);
         loadProg.setMax(100);
+
+        map=(HorizontalScrollView)findViewById(R.id.map);
+        map.setVisibility(View.INVISIBLE);
+        map.setEnabled(Boolean.FALSE);
 
         loading();      //Loading/StartScreen
 
@@ -67,10 +78,19 @@ public class MainActivity extends Activity {
 
             @Override
             public void onFinish() {
-                layout.setBackgroundColor(Color.CYAN);
+                layout.setBackgroundColor(Color.BLACK);
                 layout.removeView(loadProg);
+                map.setEnabled(Boolean.TRUE);
+                map.setVisibility(View.VISIBLE);
+                imgMap=new ImageView(context);
+               // imgMap.setMinimumWidth(3000);
+              //  imgMap.setMinimumHeight(200);
+                imgMap.setBackground(getResources().getDrawable(R.drawable.map1));
+                map.addView(imgMap);
+                map.setBackgroundColor(Color.MAGENTA);
+
                 //Kalla på Karta och kontrollera mot sparad fil vilken karta som ska laddas.
-            
+
             }
         }.start();
     }
