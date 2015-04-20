7a3b9e6af53936f6b6b1099a0bc61405b8681093
Andreas Andersson <bambismurfen@gmail.com>
2015-04-20 19:20:30 +0200

    Loading screen

---
+++ app/src/main/java/com/example/andreas/mainview/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.andreas.mainview;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.CountDownTimer;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+
+
+public class MainActivity extends Activity {
+
+    private RelativeLayout layout;
+    private ProgressBar loadProg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        layout=(RelativeLayout)findViewById(R.id.layout);
+        layout.setBackground(getResources().getDrawable(R.drawable.loader5));
+
+        loadProg=(ProgressBar)findViewById(R.id.loadProg);
+        loadProg.setMax(100);
+
+        loading();      //Loading/StartScreen
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    //********************KOD*****************
+
+    public void loading(){          //StartupScreen Loader
+        new CountDownTimer(4000,25){
+            //Sounds???
+            int progress;
+            @Override
+            public void onTick(long millisUntilFinished) {  //Update Loading Bar.
+                progress+=1;
+                loadProg.setProgress(progress);
+            }
+
+            @Override
+            public void onFinish() {
+                layout.setBackgroundColor(Color.CYAN);
+                layout.removeView(loadProg);
+                //Kalla på Karta och kontrollera mot sparad fil vilken karta som ska laddas.
+            
+            }
+        }.start();
+    }
+}
